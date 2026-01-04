d2d7fbfe2e783c413526b0a9d3be38a3d115b09c
Jonas Barros <jonasbarros98@gmail.com>
2026-01-04 20:28:28 -0300

    Move Procfile para raiz

--- config/settings.py
+++ config/settings.py
@@ -24,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%ok!u6bkj7w3ee^f9@$p=@u1rpr-fd)1knf!s=n!s-1gzbjs6c'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret-unsafe")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
